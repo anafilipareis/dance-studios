79ea95c7bc4fcf00f3c9124e5b214838fee7e42d
Ana Filipa <anafilipa737@gmail.com>
2024-03-16 12:01:22 +0000

    adding auth.services.jsx file for deployment

---
+++ DanceKingdom/src/services/auth.service.jsx
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_DEPLOYMENT_URL,
+});
+
+export const login = async (email, password) => {
+  try {
+    const response = await api.post('/auth/login', { email, password });
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const register = async (userData) => {
+  try {
+    const response = await api.post('/auth/signup', userData);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getUserProfile = async () => {
+  try {
+    const response = await api.get('/profile');
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+// Add other authentication-related functions as needed
+
+export default {
+  login,
+  register,
+  getUserProfile,
+  // Add other functions here
+};
