e72701b7e25f6c9ab574adbe55e43d4e92e50c05
Ana Filipa <anafilipa737@gmail.com>
2024-03-19 23:27:51 +0000

    adjustments to the carousel, to the nav bar

--- DanceKingdom/src/components/NavBar.jsx
+++ DanceKingdom/src/components/NavBar.jsx
@@ -1,44 +1,54 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";                    
 import { AuthContext } from "../context/auth.context"; 
-
+import logo from "../assets/logo.png"; // Import your logo image
 
 function NavBar() {
-  const { isLoggedIn, user, logOutUser } = useContext(AuthContext); 
+  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   return (
-    <nav>
-      <Link to ="/">
-        <button>Home</button>
-      </Link>
-      <Link to ="/dance-styles">
-        <button>Dance Styles</button>
-      </Link>
+    <header className="navbar">
+      <img src={logo} alt="Logo" className="logo" /> {/* Add logo */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dance-styles">Dance Styles</Link>
+          </li>
 
-      {isLoggedIn && (
-        <>
-          <span>Good to see you {user.name}</span>
-          <Link to ="/profile">
-          <button>Profile</button>
-          </Link>
-          <Link to="/dance-classes">
-          <button>Dance Classes</button>
-          </Link>
-          <button onClick={logOutUser}>Logout</button>
-        </>
-      )}
+          {isLoggedIn && (
+            <>
+              <li>
+                <span>Good to see you {user.name}</span>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/dance-classes">Dance Classes</Link>
+              </li>
+              <li>
+                <button onClick={logOutUser}>Logout</button>
+              </li>
+            </>
+          )}
 
-      {!isLoggedIn && (
-              <>
-                <Link to="/signup"> <button>Sign Up</button> </Link>
-                <Link to="/login"> <button>Login</button> </Link>
-              </>
-            )}
-
-      
-      
-    </nav>
-  )
+          {!isLoggedIn && (
+            <>
+              <li>
+                <Link to="/signup">Sign Up</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default NavBar;
