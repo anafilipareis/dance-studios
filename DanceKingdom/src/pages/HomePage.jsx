dc2f50d19dbc00614236ea0c9439a3d556e92f96
Ana Filipa <anafilipa737@gmail.com>
2024-03-28 20:39:46 +0000

    home page styling

--- DanceKingdom/src/pages/HomePage.jsx
+++ DanceKingdom/src/pages/HomePage.jsx
@@ -1,64 +1,68 @@
 import React from 'react';
-import MainCarousel from '../components/Carousel'; // Import your MainCarousel component
-import '../styles/homePageStyles.css';
+import MainCarousel from '../components/Carousel';
 import Footer from '../components/footer';
-import pricesImage from '../assets/prices.png'; // Import the prices image
+import pricesImage from '../assets/prices.png';
+import contactUs from '../assets/contactus.png';
+import danceGuy from '../assets/danceguy.png';
+import '../styles/homePageStyles.css';
 
 const HomePage = () => {
   return (
     <div className="container">
-      <MainCarousel /> {/* Render the MainCarousel component */}
-      
-      {/* About Us Section */}
+      <MainCarousel />
+
       <div className="content">
-        <div className="about-us">
-          <h2>About Us</h2>
-          <p>Dance Kingdom has been a beacon of dance excellence in Amsterdam. 
-            Over the years, our studio has been graced by extraordinary dancers who have left an indelible mark on the dance world. 
-            Our vibrant community celebrates the joy of movement and self-expression. 
-            Join us at Dance Kingdom where passion meets dance, and every step tells a story.</p>
-        </div>
+        {/* About Us and Watch Out Our Classes Section */}
+        <div className="about-us-section">
+          <div className="about-us">
+            <h2>About Us</h2>
+            <p>Dance Kingdom has been a beacon of dance excellence in Amsterdam. Over the years, our studio has been graced by extraordinary dancers who have left an indelible mark on the dance world. Our vibrant community celebrates the joy of movement and self-expression. Join us at Dance Kingdom where passion meets dance, and every step tells a story.</p>
+          </div>
 
-        {/* Watch Out Our Classes Section */}
-        <div className="watch-our-classes">
-          <h2>Watch Out Our Classes</h2>
-          {/* Embedded YouTube Video */}
-          <div className="video-container">
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/_H7CRdx7Vjk"
-              title="Watch Our Classes"
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
+          <div className="watch-our-classes">
+            <h2>Watch Out Our Classes</h2>
+            <div className="video-container">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/_H7CRdx7Vjk"
+                title="Watch Our Classes"
+                frameBorder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
           </div>
         </div>
 
-        {/* Prices Section */}
-        <div className="prices">
-          <h2>Prices</h2>
-          {/* Image for Prices section */}
-          <div className="prices-image">
-            <img src={pricesImage} alt="Prices" />
+        {/* Prices and Contact Us Section */}
+        <div className="prices-contact-section">
+          <div className="prices">
+            <div className="prices-image">
+              <img src={pricesImage} alt="Prices" style={{ width: '500px', height: 'auto' }} />
+            </div>
           </div>
-        </div>
 
-        {/* Information Section */}
-        <div className="information">
-          <h2>Information</h2>
-          {/* Information content */}
-          {/* Add your contact form or contact information here */}
+          <div className="contact-us">
+            <div className="contact-us-image">
+              <img src={contactUs} alt="Contact us" style={{ width: '400px', height: 'auto' }} />
+            </div>
+          </div>
+
+          <div className="danceguy-image-section">
+            <div className="danceguy-image">
+              <img src={danceGuy} alt="Dance Image" style={{ width: '250px', height: 'auto' }} />
+            </div>
+          </div>
         </div>
       </div>
 
-      {/* Footer Section */}
       <Footer />
     </div>
   );
 };
 
 export default HomePage;
+
 
 
